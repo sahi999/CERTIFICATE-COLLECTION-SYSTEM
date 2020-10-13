859bae330c2aa8ef6eac90bdaf3cd06a1903ab02
sahi999 <56267123+sahi999@users.noreply.github.com>
2020-10-13 19:27:28 +0530

    Add files via upload

--- Django Project/CCSP/Certificate/views.py
+++ Django Project/CCSP/Certificate/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from Certificate.models import Login
-from Certificate.forms import DocumentForm 
+from Certificate.models import Login,Document
+from Certificate.forms import DocumentForm
+import csv
+
 
 # Create your views here.
 def base(request):
@@ -21,8 +23,22 @@ def uploadhtml(request):
 		form=DocumentForm(request.POST,request.FILES)
 		if form.is_valid():
 			form.save()
-			return redirect('uploadhtml')
+			return redirect('showdocument')
 	else :
 		form=DocumentForm()
 	return render(request,'Certificate/uploadhtml.html',{'form':form})
-	     		
+
+def showdocument(request):
+	docs=Document.objects.all()
+	return render(request,"Certificate/showdocument.html",{'document':docs})
+
+def exportcsv(request):
+	details=Login.objects.all()
+	response=HttpResponse('text/csv')
+	response['Content-Disposition']='attachment;filename=login.csv'
+	writer=csv.writer(response)
+	writer.writerow(['userid','pwd'])
+	fields=details.values_list('userid','pwd')
+	for i in fields:
+		writer.writerow(i)
+	return response	     		
